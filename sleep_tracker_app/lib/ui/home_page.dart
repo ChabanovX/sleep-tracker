0acfe3f3e3a5aba2868b56417cb7ece62149a437
tirex300 <thetirex29@gmail.com>
2024-06-27 15:03:59 +0300

    Basic Project Details

---
+++ sleep_tracker_app/lib/ui/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("Home"),
+      ),
+      child: Center(
+        child: Text("Home Page"),
+      ),
+    );
+  }
+}
